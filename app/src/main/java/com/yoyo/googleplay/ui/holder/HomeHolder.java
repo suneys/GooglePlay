4e2182d348be35ee962ebf539138031f05fa7d8c
yoyo <wuqingyou_w@163.com>
2016-05-31 17:58:56 +0800

    加入home界面元素

--- app/src/main/java/com/yoyo/googleplay/ui/holder/HomeHolder.java
+++ app/src/main/java/com/yoyo/googleplay/ui/holder/HomeHolder.java
@@ -1,22 +1,35 @@
 package com.yoyo.googleplay.ui.holder;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.yoyo.googleplay.R;
 import com.yoyo.googleplay.domain.AppInfo;
 import com.yoyo.googleplay.utils.UIUtils;
 
+import android.text.format.Formatter;
+
 /**
  * Created by Administrator on 2016/5/21 0021.
  */
 public class HomeHolder extends BaseHolder<AppInfo> {
-    private TextView tvContent;
+
+    private ImageView ivIcon;
+    private TextView tvName;
+    private RatingBar rbStart;
+    private TextView tvSize;
+    private TextView tvDesc;
 
     @Override
     public View initView() {
         View view = UIUtils.inflate(R.layout.list_item_home);
-        tvContent = (TextView) view.findViewById(R.id.tv_content);
+        ivIcon = (ImageView) view.findViewById(R.id.iv_icon);
+        tvName = (TextView) view.findViewById(R.id.tv_name);
+        rbStart = (RatingBar) view.findViewById(R.id.rb_start);
+        tvSize = (TextView) view.findViewById(R.id.tv_size);
+        tvDesc = (TextView) view.findViewById(R.id.tv_desc);
         return view;
     }
 
@@ -24,6 +37,9 @@ public class HomeHolder extends BaseHolder<AppInfo> {
 
     @Override
     public void refreshView(AppInfo data) {
-        tvContent.setText(data.name);
+        tvName.setText(data.name);
+        rbStart.setRating(data.stars);
+        tvSize.setText(Formatter.formatFileSize(UIUtils.getContext(),data.size));
+        tvDesc.setText(data.des);
     }
 }
